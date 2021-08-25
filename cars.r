559cbc27e2ae092c52baa936803d3754a8be357f
Edd Tempest <56262218+eddtempest@users.noreply.github.com>
2021-08-25 12:16:45 +0100

    Update cars.r
    Added basic analysis to cars data set

---
+++ cars.r
@@ -0,0 +1,15 @@
+# ggplot2 examples
+library(ggplot2)
+#use color brewer as default discrete colors
+scale_colour_discrete <- function(...) scale_color_brewer(palette="Set1", ...)
+scale_fill_discrete <- function(...) scale_fill_brewer(palette="Set1", ...)
+
+data('mtcars')
+# create factors with value labels
+mtcars$gear <- factor(mtcars$gear,levels=c(3,4,5),
+     labels=c("3gears","4gears","5gears"))
+mtcars$am <- factor(mtcars$am,levels=c(0,1),
+     labels=c("Automatic","Manual"))
+mtcars$cyl <- factor(mtcars$cyl,levels=c(4,6,8),
+   labels=c("4cyl","6cyl","8cyl"))
+head(mtcars)
